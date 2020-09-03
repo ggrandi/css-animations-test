e0069b41956ec034fd471c63f1f3c05e020908fc
pythonKiwi123 <49359646+pythonKiwi123@users.noreply.github.com>
2020-09-03 23:01:14 +0200

    added build

---
+++ build/precache-manifest.d9a932ca4bb74c4822a48170fa9deaf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5dc27f5dd843817015b604b3fa7c7d",
+    "url": "/css-animations-test/index.html"
+  },
+  {
+    "revision": "c8aafd3a32bfed4b1e42",
+    "url": "/css-animations-test/static/css/main.3df5e2cb.chunk.css"
+  },
+  {
+    "revision": "30c8a651783280aa6717",
+    "url": "/css-animations-test/static/js/2.544e7169.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/css-animations-test/static/js/2.544e7169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8aafd3a32bfed4b1e42",
+    "url": "/css-animations-test/static/js/main.cd6fc46b.chunk.js"
+  },
+  {
+    "revision": "b070b900f515a8e26486",
+    "url": "/css-animations-test/static/js/runtime-main.4a4ecd12.js"
+  }
+]);
